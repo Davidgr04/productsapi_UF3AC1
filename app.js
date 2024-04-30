70cec140f563b24da81aa444b58986d4bc0ae3e6
Davidgr04 <davidgr2004ole@gmail.com>
2024-04-30 16:35:29 +0200

    slugify_actualizacion

--- app.js
+++ app.js
@@ -1,48 +1,22 @@
-const express = require('express')
-const products_routes = require('./routes/products.js')
+const express = require('express');
+const products_routes = require('./routes/products.js');
 
-//Server instantiation
-const app = express()
+// Instancia del servidor
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-//Server configuration: template engine
+// Configuración del servidor: motor de plantillas
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static('/views'));
 
-//Midleware
-app.use(express.json())
-app.use('/', products_routes)
-
-//Server startup
-app.listen(5000, () => {
-    console.log('server is listening on port 5000')
-})
-
-// npm install mongodb
+// Middleware
+app.use(express.json());
+app.use('/', products_routes);
 
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://davidgarcia:qweQWE123@products.6i5f3ae.mongodb.net/?retryWrites=true&w=majority&appName=products";
+// Inicio del servidor
+app.listen(PORT, () => {
+    console.log(`El servidor está escuchando en el puerto ${PORT}`);
+});
 
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
